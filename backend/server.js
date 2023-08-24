4538d69db78046a474f6728dbcaea201c4d31668
Amit Rawat <amitrawat1099@gmail.com>
2023-08-24 17:48:38 +0530

    served static files

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const router = require("./routes/router");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 const connectToDB = require("./db");
 connectToDB();
 app.use(
@@ -15,6 +16,11 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1", router);
+
+app.use(express.static(path.join(__dirname, `../frontend/build`)));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, `../frontend/build/index.html`));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
